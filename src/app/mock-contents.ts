50d7b0e4c2fc0bbd7d4c0e1eeeaacd065b5367fb
yamanishiyusuke <nextcentury2000@gmail.com>
2019-03-15 00:08:04 +0900

    update layout in language page

--- src/app/mock-contents.ts
+++ src/app/mock-contents.ts
@@ -14,7 +14,7 @@ export const PROFILE: Content[] = [
 ]
 
 export const LANGUAGES: Content[] = [
-    {id: 0, title: 'C#', body: '主にUnityで使用していました。トップクラスに使い込んだ言語ですが、あくまでもUnityに特化しており、windows向けはしたことないという…', footer: '使用歴：2年' },
+    { id: 0, title: 'C#', body: '主にUnityで使用していました。トップクラスに使い込んだ言語ですが、あくまでもUnityに特化しており、windows向けはしたことないという…', footer: '使用歴：2年' },
     { id: 1, title: 'Java', body: 'Androidで使用していました。最初の頃で非常に覚えるのに苦労した覚えが。今となってはいい思い出です。', footer: '使用歴：2年' },
     { id: 2, title: 'PHP', body: 'API作ったり、今はWebサービス開発でがっつり使ってます。フレームワークはCakePHP使用。', footer: '使用歴：2年' },
     { id: 3, title: 'Kotlin', body: 'kotlinがandroidに採用されて使ってみることに。そしたら書きやすくて現在僕の中でトップクラスに評価が高い言語になる。型推論の軽やかさと安全性を両立していて好感触。', footer: '使用歴：6ヶ月' },
