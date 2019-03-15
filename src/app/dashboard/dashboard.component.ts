c1363b21fa3253223e936242162306cd0c4d1758
yamanishiyusuke <nextcentury2000@gmail.com>
2019-03-15 18:50:32 +0900

    add animation component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Menu } from '../menu';
 import { ContentsService } from '../contents.service';
+import { trigger, state, style, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        backgroundColor: 'gray',
+      })),
+      state('colsed', style({})),
+      transition('open => closed', [
+        
+      ])
+    ])
+  ]
 })
 export class DashboardComponent implements OnInit {
   menus: Menu[] = [];
